9dc48a76f0b5d1818c7ce250e1bbf73d3471b0b9
Alex Voß <alex@storysh-media.de>
2025-06-28 22:09:01 +0200

    feat: update vue module instaciation

--- assets/js/modules/dynamic-form.js
+++ assets/js/modules/dynamic-form.js
@@ -1,10 +1,8 @@
 import DynamicForm from '../components/elements/plugin/form/DynamicForm.vue';
 import VueModule from '@core/js/modules/vue-module.js';
 
-VueModule.observeAndMount({
-  identifier: 'dynamic-form',
-  component: DynamicForm,
-  options: {useStore: true, provideDataset: true, identifier: 'dynamic-form'}
-}).then(({ app, section }) => {
-  console.log('Vue erfolgreich gemountet auf:', section);
-}).catch(console.error);
+VueModule.init(
+  'dynamic-form',
+  DynamicForm,
+  {useStore: true, provideDataset: true, identifier: 'dynamic-form'}
+);
